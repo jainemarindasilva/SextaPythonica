5e85caae675358248e9d805c191941fb6cfb4848
JAINE MARIN DA SILVA <52661791+jainemarindasilva@users.noreply.github.com>
2021-10-29 11:33:19 -0300

    Create numbers.py

---
+++ learning_the_basics/01/numbers.py
@@ -0,0 +1,19 @@
+number:int = 123
+number = 123
+number2:float = 5.63
+number2 = 5.63
+
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2  retorna um float
+# Floor Division: 3 // 2 retorna sempre o inteiro da divisão
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
